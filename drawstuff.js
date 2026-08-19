59563ac19a9422a7ee9ff1068b92bf3436b68c7d
cpcervan <cervantes.christian86@gmail.com>
2026-08-19 14:42:14 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -79,8 +79,8 @@ function main() {
  
     // Draw a rectangle with pixels
     var c = new Color(0,255,255,255); // the color at the pixel: black opaque
-    for (var x=250; x<900; x++) 
-        for (var y=150; y<675; y++) {
+    for (var x=50; x<150; x++) 
+        for (var y=50; y<175; y++) {
             drawPixel(imagedata,x,y,c);
             // console.log("draw at " +x+ " " +y);
         }
